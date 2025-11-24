d5ccd3e68d1b94c190c607c3526aa628e0806c65
BinKami <kakoro@op.pl>
2025-11-25 00:14:18 +0100

    2.

---
+++ boolean-type/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean isUserLoggedIn = false; // TAK/NIE
+        boolean startWithK = "Kamil".startsWith("K");
+
+        int a = 5;
+        boolean numberGreaterThan = a > 10;
+
+        System.out.println(numberGreaterThan);
+
+    }
+}
